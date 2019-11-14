d3602e681c556c00039406be57322dca7c38ebae
wuzhangsheng <dasheng253@163.com>
2019-11-14 15:13:25 +0800

    subsets: loop

--- src/main/java/leetcode/subsets/Solution.java
+++ src/main/java/leetcode/subsets/Solution.java
@@ -1,6 +1,7 @@
 package leetcode.subsets;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -44,4 +45,25 @@ public class Solution {
         addToList.add(nums[i]);
         subs(res, addToList, nums, i + 1);
     }
+
+    // 子集的产生，包含以下规律：
+    // [1,2,3] 首先要包含空集[]，再取出第一个元素1,加到已有的子集中得到[1]，与之前空集组成新的结果：[ [],[1] ]
+    // 再取出元素2，在已有结果的所有子集中加入2，得到两个子集[2],[1,2]，生成新的结果[ [],[1],[2],[1,2] ]
+    // 再取3，得到[ [],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3] ]
+    public List<List<Integer>> subsets2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> tmp = new ArrayList<>();
+        res.add(new ArrayList<>());
+        Arrays.stream(nums).forEach(i -> {
+            res.forEach(list -> {
+                List<Integer> toAdded = new ArrayList<>(list);
+                toAdded.add(i);
+                tmp.add(toAdded);
+            });
+            res.addAll(tmp);
+            tmp.clear();
+        });
+        return res;
+    }
+
 }
