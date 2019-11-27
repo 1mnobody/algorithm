ee53ddd35cc2b430fcc80965b3cfabc33b9dc4df
1mnobody <dasheng253@163.com>
2019-11-28 00:22:24 +0800

    leetcode binary tree right side view : dfs solution using Hashmap

--- src/main/java/leetcode/binary/tree/right/side/view/Solution.java
+++ src/main/java/leetcode/binary/tree/right/side/view/Solution.java
@@ -1,9 +1,6 @@
 package leetcode.binary.tree.right.side.view;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by wuzhsh on 11/27/2019.
@@ -35,6 +32,23 @@ public class Solution {
             }
         }
         return res;
+    }
+
+    public List<Integer> rightSideView2(TreeNode root) {
+        Map<Integer, Integer> map = new HashMap<>();
+        dfs(root, map, 0);
+        return new ArrayList<>(map.values());
+    }
+
+    //  这里使用了map，先遍历左边的节点，再遍历右边的节点，同一层级的左边节点会被右边节点覆盖
+    // leetcode上还有使用 list 直接存储的，这种情况下，就是先遍历右边节点，将相关的数据填充到list（list.size表示当前层级，
+    // 所以只加最后一个元素），再遍历左节点，完成更下层节点对应数据的填充
+    public void dfs(TreeNode node, Map<Integer, Integer> map, int level) {
+        if (node != null) {
+            map.put(level, node.val);
+            dfs(node.left, map, level + 1);
+            dfs(node.right, map, level + 1);
+        }
     }
 }
 
